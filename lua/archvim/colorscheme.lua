bd2ce1dfd380f7c44bd9cfdfb5557260a6b32b04
GZHD <gaozhuohang107@gmail.com>
2026-07-07 10:29:58 +0800

    fix: switch to tokyonight-storm (dark blue theme)

--- lua/archvim/colorscheme.lua
+++ lua/archvim/colorscheme.lua
@@ -1,26 +1,23 @@
--- 当前使用 tokyonight-day (浅色蓝调)
+-- 当前使用 tokyonight-storm (深色蓝调)
 -- 其他可选 colorscheme:
 --   :colorscheme tokyonight-night    (深沉暗色)
---   :colorscheme tokyonight-storm    (蓝调暗色)
 --   :colorscheme tokyonight-moon     (紫调暗色)
+--   :colorscheme tokyonight-day      (浅色蓝调)
 --   :colorscheme gruvbox             (复古暖色)
 --   :colorscheme catppuccin-mocha    (暖色暗色)
---   :colorscheme catppuccin-latte    (浅色)
+--   :colorscheme catppuccin-frappe   (柔和暗蓝)
 --   :colorscheme nord                (冷色蓝调)
 
-local colorscheme = "tokyonight-day"
+local colorscheme = "tokyonight-storm"
 
 -- 配置 tokyonight
 require("tokyonight").setup({
-    style = "day",
+    style = "storm",
     transparent = require'archvim.options'.transparent_color,
     styles = {
         comments = { italic = true },
         keywords = { italic = false },
     },
-    on_colors = function(colors)
-        -- 可以在这里微调颜色
-    end,
 })
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
